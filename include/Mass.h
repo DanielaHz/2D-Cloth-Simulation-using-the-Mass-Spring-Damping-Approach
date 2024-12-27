face0987afaac36d0225bc8eb11f70d96a304c98
Daniela Hernandez <yessicadanielahernandez@gmail.com>
2024-12-27 15:54:55 +0000

    feat: basic mass class implementation

--- include/Mass.h
+++ include/Mass.h
@@ -1,4 +1,14 @@
 #ifndef MASS_H_
 #define MASS_H_
+#include <ngl/Vec3.h>
+class Mass {
+    public:
+        ngl::Vec3 position;
+        ngl::Vec3 velocity;
+        float mass = 1.0f; 
+        float size = 5.0f;
 
+        Mass () = default;
+        ~Mass () = default; 
+};
 #endif
