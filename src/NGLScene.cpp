357800adef9ec6cc66f76b95b3505b3026456fb9
DanielaHz <yessicadanielahernandez@gmail.com>
2025-01-10 15:55:03 +0000

    feat: window background color changed

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -35,7 +35,7 @@ void NGLScene::initializeGL()
   // we must call that first before any other GL commands to load and link the
   // gl commands from the lib, if that is not done program will crash
   ngl::NGLInit::initialize();
-  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);			   // black Background or white?
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);			   // black Background or white?
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
