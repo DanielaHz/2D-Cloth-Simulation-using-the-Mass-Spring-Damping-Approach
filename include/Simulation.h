3616598e9566b3ca711f4652568f32ca0d8a1171
DanielaHz <yessicadanielahernandez@gmail.com>
2025-01-11 13:58:41 +0000

    feat: simulation class updated

--- include/Simulation.h
+++ include/Simulation.h
@@ -1,15 +1,23 @@
 #ifndef SIMULATION_H_
 #define SIMULATION_H_
 
-# include "Cloth.h"
+#include "Cloth.h"
+#include <chrono>
 
-// for the moment his class only implement the functions created in Cloth as draw and init
+
 class Simulation {
 public:
     Simulation (int width, int height, float spacing);
+    void initialize();
     void render();
+    void update(float deltaTime);
+    void start();
+    void simulationLoop();
 private:
     Cloth m_cloth;
+    std::chrono::steady_clock::time_point m_lastTime;
+    float m_time;
+
 };
 
 #endif
--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,11 +1,46 @@
 #include "Simulation.h"
 
 Simulation::Simulation(int width, int height, float spacing)
+    : m_lastTime(std::chrono::steady_clock::now()), m_time(0.0f) 
 {
-    m_cloth.initCloth(width, height, spacing);
+    m_cloth.initCloth(width,height, spacing);
+}
+
+void Simulation::initialize()
+{
+    // should initalize the forces constants here and not declare it in cloth?
+    m_time = 0.0f;
+}
+
+void Simulation::update(float deltaTime)
+{
+    m_cloth.evaluateForces();
+    m_cloth.requestNewState(m_time , deltaTime);
+    m_time += deltaTime;
 }
 
 void Simulation::render()
 {
     m_cloth.drawCloth();
+}
+
+void Simulation::start()
+{
+    m_lastTime = std::chrono::steady_clock::now();
+    simulationLoop();
+}
+
+void Simulation::simulationLoop() {
+    while (true) {
+        auto currentTime = std::chrono::steady_clock::now();
+        std::chrono::duration<float> elapsedTime = currentTime - m_lastTime;
+        float deltaTime = elapsedTime.count();
+
+        if (deltaTime >= 0.05f) 
+        {
+            update(0.05f);
+            render();
+            m_lastTime = currentTime;
+        }
+    }
 }
